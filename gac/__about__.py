21db1a0863abad1d4f460a042b0a7bdfea8367a8
Robert de Vries <r.devries@robbytu.net>
2016-03-29 01:51:17 +0200

    + Added initial code for the gac framework package

---
+++ gac/__about__.py
@@ -0,0 +1,6 @@
+__title__ = 'gac framework'
+__version_info__ = ('1', '0', '0')
+__version__ = '.'.join(__version_info__)
+__author__ = 'Gretha\'s Ambachtelijke Codefabriek'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Gretha\'s Ambachtelijke Codefabriek'
---
+++ gac/__init__.py
@@ -0,0 +1,9 @@
+"""
+gac framework
+-------------
+
+Gretha's Ambachtelijke Codefabriek ontwikkeld een Framework voor bordspellen.
+Informatica Thema 2.3 Software Engineering
+"""
+
+from .__about__ import (__title__, __version_info__, __version__, __author__, __license__, __copyright__)
