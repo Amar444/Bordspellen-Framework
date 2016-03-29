cbf831abec9ff57937028842f1cfa032f4894bd6
Robert de Vries <r.devries@robbytu.net>
2016-03-29 02:15:02 +0200

    + Added util functions to get the faulty coordinates from a InvalidCoordinatesException exception

--- gac/exceptions.py
+++ gac/exceptions.py
@@ -1,2 +1,17 @@
+"""
+This file contains all exceptions that may be thrown by the GAC framework
+"""
+
 class InvalidCoordinatesException(Exception):
-    pass
+    """ Indicates invalid coordinates """
+
+    coordinates = ()
+
+    def __init__(self, x: int, y: int):
+        """ Initializes a new InvalidCoordinatesException with the given coordinates """
+        super().__init__("The given coordinates ({}, {}) are invalid for this board.".format(x, y))
+        self.coordinates = (x, y)
+
+    def get_coordinates(self):
+        """ Returns the faulty coordinates that caused this Exception to occur """
+        return self.coordinates
