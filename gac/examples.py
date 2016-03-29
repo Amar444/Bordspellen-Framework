a0fefe6447a64ec30f315c8abb9df4bafc452ed6
Frank Noorlander <F.noorlander1@gmail.com>
2016-03-29 17:31:33 +0200

    * Refactored/abstracted code for players.

--- gac/examples.py
+++ gac/examples.py
@@ -1,23 +1,14 @@
 from games import *
 from boards import TwoDimensionalBoard
-from players import BoardPlayerMixin, NamedPlayerMixin
+from players import BoardPlayerMixin, NamedPlayerMixin, CommandLineInputPlayerMixin
 
 class DemoBoard(TwoDimensionalBoard):
     size = (10, 5)
 
 
-class DemoPlayer(BoardPlayerMixin, NamedPlayerMixin):
+class DemoPlayer(BoardPlayerMixin, NamedPlayerMixin, CommandLineInputPlayerMixin):
     def play(self):
         super().play()
-
-        try:
-            coords = input("Please enter coords to update the board? [x,y] ")
-            x, y = coords.split(',')
-            self.board.set(int(x), int(y), self.name[0:1])
-            print("\n")
-        except Exception as e:
-            print(e)
-            self.play()
 
 
 class DemoGame(TurnBasedGame, BoardGame):
