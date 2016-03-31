7df2fead476a3f81ca5397246dd9698adc97bcbd
johankladder <j.kladder@st.hanze.nl>
2016-03-31 21:11:41 +0200

    ADD: Removed tabs -> Only view from now on

--- gui/Browser.py
+++ gui/Browser.py
@@ -1,60 +1,26 @@
 import sys
 from gi.repository import Gtk, Gdk, WebKit
 
-
-class BrowserTab(Gtk.VBox):
-    def __init__(self, *args, **kwargs):
-        super(BrowserTab, self).__init__(*args, **kwargs)
-
-        self.webview = WebKit.WebView()
-        self.show()
-
-        scrolled_window = Gtk.ScrolledWindow()
-        scrolled_window.add(self.webview)
-
-        self.pack_start(scrolled_window, True, True, 0)
-
-        scrolled_window.show_all()
-
-    def _load_url_string(self, url):
-        if not "://" in url:
-            url = "http://" + url
-        self.webview.load_uri(url)
-
-
-
 class Browser(Gtk.Window):
     def __init__(self, *args, **kwargs):
         super(Browser, self).__init__(*args, **kwargs)
 
-        # create notebook and tabs
-        self.notebook = Gtk.Notebook()
-        self.notebook.set_scrollable(True)
+        webView = WebKit.WebView()
+        webView.load_uri("http://www.google.nl")
+
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.add(webView)
+        scrolled_window.show_all()
+
+        self.add(scrolled_window)
 
         self.set_size_request(700, 600)
 
-        # create a first, empty browser tab
-        self.notebook.append_page(self._create_tab())
-
-        self.add(self.notebook)
-
-        # connect signals
-        self.connect("destroy", Gtk.main_quit)
-
-        self.notebook.show()
-        self.show()
-
-
-    def _create_tab(self):
-        tab = BrowserTab()
-        tab._load_url_string("google.nl")
-        return tab
 
 
 
 if __name__ == "__main__":
-    Gtk.init(sys.argv)
-
-    browser = Browser()
-
+    win = Browser()
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
     Gtk.main()
