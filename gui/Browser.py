f1bd5c7796fb3c418b5190c64f657d02745542ee
johankladder <j.kladder@st.hanze.nl>
2016-03-31 22:38:30 +0200

    ADD: Test html files and refactored browser

--- gui/Browser.py
+++ gui/Browser.py
@@ -1,22 +1,40 @@
-import sys
-from gi.repository import Gtk, Gdk, WebKit
+import os
+from gi.repository import Gtk, WebKit
+
 
 class Browser(Gtk.Window):
     def __init__(self, *args, **kwargs):
         super(Browser, self).__init__(*args, **kwargs)
 
+        # Set title application
+        self.set_title("GameView GAC")
+
+        # Init the web-view
         webView = WebKit.WebView()
-        webView.load_uri("http://www.google.nl")
 
+        # Gets project dir:
+        BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+        # Add folder to project dir:
+        BASE_DIR += "/gui/local"
+
+        # Join begin file to project dir:
+        BASE_DIR = os.path.join(BASE_DIR, 'index.html')
+
+        # Open the file on given location in a web-view:
+        webView.open('file://' + BASE_DIR)
+
+        # Create a scrollable window:
         scrolled_window = Gtk.ScrolledWindow()
-        scrolled_window.add(webView)
-        scrolled_window.show_all()
 
+        # Add web-view to the scrollable window:
+        scrolled_window.add(webView)
+
+        # Add the scrollable window to the main window:
         self.add(scrolled_window)
 
+        # Set minimal size window:
         self.set_size_request(700, 600)
-
-
 
 
 if __name__ == "__main__":
