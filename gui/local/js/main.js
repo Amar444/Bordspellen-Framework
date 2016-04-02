35aa970cf3ac736e9120e6524031fd82ad62a7a3
Mark Nijboer <mark@Marks-Mac.local>
2016-04-02 19:29:22 +0200

    + GUI: Sound, challenge page

--- gui/local/js/main.js
+++ gui/local/js/main.js
@@ -1,3 +1,57 @@
+/* Application */
+var app = {
+  "_utilities" : {
+    "fadeOutVolume" : function(audio) {
+      var vol = audio.volume;
+      var fadeout = setInterval(
+        function() {
+          // Reduce volume by 0.05 as long as it is above 0
+          // This works as long as you start with a multiple of 0.05!
+          if (vol > 0) {
+            vol -= 0.05;
+            audio.volume = vol;
+          }
+          else {
+            // Stop the setInterval when 0 is reached
+            clearInterval(fadeout);
+          }
+        }, 50);
+      },
+      "fadeInVolume" : function(audio) {
+        var vol = audio.volume;
+        var fadeout = setInterval(
+          function() {
+            // Reduce volume by 0.05 as long as it is above 0
+            // This works as long as you start with a multiple of 0.05!
+            if (vol < 1) {
+              vol += 0.05;
+              audio.volume = vol;
+            }
+            else {
+              // Stop the setInterval when 0 is reached
+              clearInterval(fadeout);
+            }
+          }, 50);
+        }
+    },
+  "main" : {
+    "unloadPage" : function(dest, button) {
+      view.frameContent.classList.add('hideElem');
+      view.frameBackground.classList.add('hideElem');
+
+      app._utilities.fadeOutVolume(window.playAudio);
+
+      setTimeout(function() {
+        if(sessionStorage != null) {
+          sessionStorage.musicTime = window.playAudio.currentTime;
+        }
+        window.location=dest;
+      }, 1000);
+    }
+  }
+};
+
+
 /* Initialize */
 
 var view = {
@@ -10,15 +64,17 @@ var view = {
 view.frameContent.classList.remove('hideElem');
 view.frameBackground.classList.remove('hideElem');
 
-/* Application */
-var app = {
-  "main" : {
-    "unloadPage" : function(dest, button) {
-      view.frameContent.classList.add('hideElem');
-      view.frameBackground.classList.add('hideElem');
-      setTimeout(function() {
-        window.location=dest;
-      }, 1000);
-    }
-  }
-};
+window.playAudio = new Audio('sounds/sound.mp3');
+
+window.playAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+
+if(sessionStorage != null && window.home != true) {
+  window.playAudio.currentTime = sessionStorage.musicTime;
+}
+
+window.playAudio.volume = 0;
+app._utilities.fadeInVolume(window.playAudio);
+window.playAudio.play();
