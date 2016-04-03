9926271aa643ffc6a82b1f10e3913552c8df68c1
JannesJonkers <jjonkers2@gmail.com>
2016-04-03 17:05:40 +0200

    *Changed is_legal_move to look for opponent(s) followed by self instead of empty (oops)

--- reversi/game.py
+++ reversi/game.py
@@ -1,54 +1,54 @@
 """ Provides tools to enforce game rules and keep track of the game """
 
-from gac.boards import TwoDimensionalBoard
-from gac.games import BoardGame, TurnBasedGame
+from boards import TwoDimensionalBoard
+from games import BoardGame, TurnBasedGame
 
 REVERSI_BOARD_SIZE = 8
 
-#Represtents the 8 directions, N, S, E, W, NW, NE, SW, SE in no particular order
-DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1 ,-1), (1, 0), (1,1)]
+# Represtents the 8 directions, N, S, E, W, NW, NE, SW, SE in no particular order
+DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
 
 class ReversiBoard(TwoDimensionalBoard):
     """ Represents an reversi board"""
 
     size = (REVERSI_BOARD_SIZE, REVERSI_BOARD_SIZE)
 
+
 class ReversiGame(TurnBasedGame, BoardGame):
     """ Represents Reversi game"""
     board_class = ReversiBoard
 
-
     def is_legal_move(self, player: any, row: int, col: int):
         """Determine if the play on a square is an legal move"""
-        if self.board.isAvaiable() is False:
+        if self.board.is_available(row, col) is False:
             return False
         for direction in range(len(DIRECTIONS)):
             spotted_opponent = False
             for distance in range(REVERSI_BOARD_SIZE):
-                row_to_check = row + distance * DIRECTIONS[direction][0]
-                col_to_check = col + distance * DIRECTIONS[direction][1]
+                row_to_check = row + (distance+1) * DIRECTIONS[direction][0]
+                col_to_check = col + (distance+1) * DIRECTIONS[direction][1]
                 try:
-                    self.board.checkCoordinates(row_to_check, col_to_check)
+                    self.board.check_coordinates(row_to_check, col_to_check)
                     stone = self.board.get(row_to_check, col_to_check)
-                    if stone == player:
+                    if stone is None:
                         break
-                    elif stone is None and spotted_opponent == False:
+                    elif stone == player and spotted_opponent == False:
                         break
-                    elif stone is None and spotted_opponent:
+                    elif stone == player and spotted_opponent:
                         return True
                     else:
                         spotted_opponent = True
-                except:
+                except Exception as e:
+                    # print(e)
                     break
         return False
-
-
 
     def get_legal_moves(self, player: any):
         """ Functions that figures out whic legal moves there currently are for the player"""
         moves = []
         for row in range(REVERSI_BOARD_SIZE):
             for col in range(REVERSI_BOARD_SIZE):
-                if self.is_legal_move():
-                    moves.append((row,col))
+                if self.is_legal_move(player, row, col) is True:
+                    moves.append((row, col))
         return moves
