ba56e090638d3f2765d2504b29c3edc8d55da0b7
Robert de Vries <r.devries@robbytu.net>
2016-04-03 15:55:01 +0200

    * Multi-player implementation for the simple client

--- gac/examples/simple_client.py
+++ gac/examples/simple_client.py
@@ -1,9 +1,20 @@
-from client import Client, EVENT_CONNECTED
+from time import sleep
+from client import Client, EVENT_CONNECTED, OutgoingCommand
 
 print("Warning: this will loop forever waiting for incoming messages")
 print("from the server. You may want to stop this script using Ctrl-C")
 print("")
 
-client = Client()
-client.connect()
-client.on(EVENT_CONNECTED, lambda m: client.login('Robert'))
+
+class PlayerClient(Client):
+    def __init__(self, nickname):
+        super().__init__()
+        self.on(EVENT_CONNECTED, self.on_connected)
+        self.nickname = nickname
+
+    def on_connected(self, data):
+        self.send(OutgoingCommand('LOGIN', self.nickname))
+
+
+PlayerClient("Kwieb").connect()
+PlayerClient("Robert").connect()
