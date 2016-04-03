953ff84c888165d60c7e0fb4c2d81fd11638a5ff
Robert de Vries <r.devries@robbytu.net>
2016-04-03 19:01:35 +0200

    * Huge refactor for the TicTacToe game. Might refactor some of the AI later on.

--- tictactoe/ai.py
+++ tictactoe/ai.py
@@ -1,46 +1,59 @@
+from game import STATUS_WINNER, STATUS_DRAW
 from players import BoardPlayerMixin, NamedPlayerMixin
 from utils import Best
 
 
 class AIPlayer(BoardPlayerMixin, NamedPlayerMixin):
+    opponent = None
+
     def __init__(self, game, *args, **kwargs):
-        """ Initializes a new game and sets up the board class """
         super().__init__(*args, **kwargs)
         self.game = game
 
     def play(self):
-        best_move = self.choose_move(self.name)
-        self.board.set(best_move.row, best_move.column, self.name)
+        if self.opponent is None:
+            for player in self.game.players:
+                if player != self:
+                    self.opponent = player
+                    break
 
-    def choose_move(self, side):
+        if self.board.last_turn is None:
+            return self.board.set(1, 1, self)
+
+        best_move = self.choose_move(self)
+        self.board.set(best_move.row, best_move.column, self)
+
+    def choose_move(self, player):
         """ Find best move for winning the game """
         best_row = 0
         best_column = 0
 
-        simple_eval = self.game.position_value()
-        if simple_eval != self.board.unclear:
-            return Best(simple_eval)
+        status, winner = self.game.state
+        if status == STATUS_WINNER:
+            return Best(3 if winner == self else 0)
+        elif status == STATUS_DRAW:
+            return Best(1)
 
         # select opponent and value
-        if side == self.name:
-            opp, value = (self.game.players[0].name, self.board.opp_win)
+        if player == self:
+            opp, value = (self.opponent, 0)
         else:
-            opp, value = (self.name, self.board.ai_win)
+            opp, value = (self, 3)
 
         # look for best move
         for col in range(3):
             for row in range(3):
                 if self.board.is_available(row, col):
                     # move to this square
-                    self.board.set(row, col, side)
+                    self.board.set(row, col, player)
                     # continue playing
                     reply = self.choose_move(opp)
                     # clear position just used
                     self.board.set(row, col, None)
 
                     # check if current player is winning
-                    if (side == self.name and reply.val > value) or \
-                            (side == self.game.players[0].name and reply.val < value):
+                    if (player == self and reply.val > value) or \
+                            (player == self.opponent and reply.val < value):
                         # current player is winning
                         value = reply.val
                         # coordinates best move
--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,104 +1,76 @@
 from boards import TwoDimensionalBoard
 from games import BoardGame, TurnBasedGame
 
+STATUS_UNCLEAR = 0
+STATUS_WINNER = 1
+STATUS_DRAW = 2
+
 
 class TicTacToeBoard(TwoDimensionalBoard):
-    """ Represents TicTacToe board """
-
-    _ai_win = 3
-    _opp_win = 0
-    _draw = 1
-    _unclear = 2
-
-    @property
-    def ai_win(self):
-        return type(self)._ai_win
-
-    @property
-    def opp_win(self):
-        return type(self)._opp_win
-
-    @property
-    def draw(self):
-        return type(self)._draw
-
-    @property
-    def unclear(self):
-        return type(self)._unclear
-
-    size = 3, 3
+    """ Represents a TicTacToe board """
+    size = (3, 3)
 
 
 class TicTacToeGame(TurnBasedGame, BoardGame):
     """ Represents TicTacToe game """
-    board_class = TicTacToeBoard
+    board = TicTacToeBoard()
+
+    @property
+    def state(self):
+        if self.board.is_full():
+            return STATUS_DRAW, None
+        else:
+            winner = self.winner
+            if winner is None:
+                return STATUS_UNCLEAR, None
+            return STATUS_WINNER, winner
+
+    @property
+    def status(self):
+        return self.state[0]
+
+    @property
+    def winner(self):
+        if self.has_won(self.players[0]):
+            return self.players[0]
+        elif self.has_won(self.players[1]):
+            return self.players[1]
+        else:
+            return None
+
+    def has_won(self, player):
+        """ Returns whether 'side' has won in this position """
+        return self.has_won_horizontal(player) or self.has_won_vertical(player) or self.has_won_diagonal(player)
+
+    def has_won_horizontal(self, player):
+        """ Check horizontal win for the given player """
+        winning = (player,)*3
+        return (self.board.get(0, 0), self.board.get(0, 1), self.board.get(0, 2)) == winning or \
+               (self.board.get(1, 0), self.board.get(1, 1), self.board.get(1, 2)) == winning or \
+               (self.board.get(2, 0), self.board.get(2, 1), self.board.get(2, 2)) == winning
+
+    def has_won_vertical(self, player):
+        """ Check vertical win for the given player """
+        winning = (player,)*3
+        return (self.board.get(0, 0), self.board.get(1, 0), self.board.get(2, 0)) == winning or \
+               (self.board.get(0, 1), self.board.get(1, 1), self.board.get(2, 1)) == winning or \
+               (self.board.get(0, 2), self.board.get(1, 2), self.board.get(2, 2)) == winning
+
+    def has_won_diagonal(self, player):
+        """ Check diagonal win for the given player """
+        winning = (player,)*3
+        return (self.board.get(0, 0), self.board.get(1, 1), self.board.get(2, 2)) == winning or \
+               (self.board.get(0, 2), self.board.get(1, 1), self.board.get(2, 0)) == winning
 
     def next_turn(self):
+        """ Check, for each turn, whether someone has won already """
         super().next_turn()
-        if self.game_over():
-            print("Game over, {} wins".format(self.winner()))
-            self.is_playing = False
-
-    def position_value(self):
-        """ Compute static value of current position (win, draw, etc.) """
-        if self.is_a_win(self.players[1].name):
-            return self.board.ai_win
-        elif self.is_a_win(self.players[0].name):
-            return self.board.opp_win
-        elif self.board.is_full():
-            return self.board.draw
-        else:
-            return self.board.unclear
-
-    def is_a_win(self, side):
-        """ Returns whether 'side' has won in this position """
-        return self.is_a_win_horizontal(side) or self.is_a_win_vertical(side) or self.is_a_win_diagonal(side)
-
-    def is_a_win_diagonal(self, side):
-        """ Check diagonal win """
-        # left top corner to right bottom corner
-        if self.board.get(0, 0) == side and self.board.get(1, 1) == side and \
-                self.board.get(2, 2) == side:
-            return True
-        # right top corner to left bottom corner
-        if self.board.get(0, 2) == side and self.board.get(1, 1) == side and \
-                self.board.get(2, 0) == side:
-            return True
-        return False
-
-    def is_a_win_vertical(self, side):
-        """ Check vertical win """
-        is_a_win_vertical = False
-        for col in range(3):
-            if is_a_win_vertical is True:
-                break
-            temp = True
-            for row in range(3):
-                if self.board.get(row, col) != side:
-                    temp = False
-                is_a_win_vertical = temp
-        return is_a_win_vertical
-
-    def is_a_win_horizontal(self, side):
-        """ Check horizontal win """
-        is_a_win_horizontal = False
-        for row in range(3):
-            if is_a_win_horizontal is True:
-                break
-            temp = True
-            for col in range(3):
-                if self.board.get(row, col) != side:
-                    temp = False
-                is_a_win_horizontal = temp
-        return is_a_win_horizontal
-
-    def game_over(self):
-        return self.position_value() != self.board.unclear
-
-    def winner(self):
-        if self.position_value() == self.board.ai_win:
-            return self.players[1].name
-        elif self.position_value() == self.board.opp_win:
-            return self.players[0].name
-        else:
-            return "nobody"
+        status = self.status
+        if status != STATUS_UNCLEAR:
+            print(self.board)
+            if status == STATUS_WINNER:
+                self.is_playing = False
+                print("Game over! Player {} has won this round!".format(self.winner))
+            elif status == STATUS_DRAW:
+                self.is_playing = False
+                print("Game over! It's a tie!")
