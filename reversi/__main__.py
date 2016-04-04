d1f146efa287bcbdc7410d83e42fce0406bc8a34
JannesJonkers <jjonkers2@gmail.com>
2016-04-04 14:45:42 +0200

    +added a base command line reversi game

--- reversi/__main__.py
+++ reversi/__main__.py
@@ -1,19 +1,34 @@
 """ This class provides the setup for an ReversiGame"""
 
 from game import ReversiGame
-from ai import AIPlayer, DemoReversiPlayer
+# from ai import AIPlayer
+from players import BoardPlayerMixin, NamedPlayerMixin, Player
+
+
+class DemoCliPlayer(NamedPlayerMixin, BoardPlayerMixin, Player):
+    def play(self):
+        super().play()
+
+        try:
+            x, y = str(input("Please enter coords to update the board? [x,y] ")).split(',')
+            game.execute_move(self.name[0:1], int(x), int(y))
+            print("\n")
+        except Exception as e:
+            print("{}\n".format(e))
+            self.play()
+
 
 game = ReversiGame()
-players = ()
-
+players = (DemoCliPlayer(name="W", board=game.board),)
 
 answer = str(input("Would you like to play against the computer? y/n"))
 if answer == "y":
-    players += (DemoReversiPlayer(name="u", board=game.board),)
-    players += (AIPlayer(game, name="c", board=game.board),)
+    print("Tough luck, that's not yet implemented.")
+    exit()
+    players += (AIPlayer(name="B", board=game.board, game=game),)
 else:
-    players += (DemoReversiPlayer(name="1", board=game.board),)
-    players += (DemoReversiPlayer(name="2", board=game.board),)
+    players += (DemoCliPlayer(name="B", board=game.board),)
 
 game.set_players(players)
+game.init_board(players[0], players[1])
 game.play()
