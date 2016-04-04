8110a1cf84b8bcf4e0ea3762898f62cc76320387
johankladder <j.kladder@st.hanze.nl>
2016-04-04 22:52:13 +0200

    + getPlayerList + getGameList

--- gui/local/js/protocolparser.js
+++ gui/local/js/protocolparser.js
@@ -25,9 +25,20 @@ var protocol = {
           return protocol;
         }
         return null;
+    },
+    "getPlayerList" :function() {
+      return protocol.parser.getProtocol("playerlist");
+    },
+    "getProtocol": function(name) {
+      if(name != null && name != "") {
+        return "get " + name;
+      } else {
+        return null;
+      }
     }
   }
 }
 
 console.log(protocol.parser.createChallenge("nameplayer", "namegame", 5));
 console.log(protocol.parser.createLogin("nameplayer"));
+console.log(protocol.parser.getPlayerList());
