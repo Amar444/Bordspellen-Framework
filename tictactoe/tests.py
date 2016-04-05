02fa8721c3d76c6950308ecb275bfb381b7d8c40
Robert de Vries <r.devries@robbytu.net>
2016-04-05 22:34:16 +0200

    * Small refactor of the unit tests for TicTacToe

--- tictactoe/tests.py
+++ tictactoe/tests.py
@@ -3,7 +3,7 @@ Unit Tests for the TicTacToe implementation
 """
 
 import unittest
-from game import TicTacToeGame
+from game import TicTacToeGame, STATUS_WINNER, STATUS_UNCLEAR, STATUS_DRAW
 from players import Player, BoardPlayerMixin, NamedPlayerMixin
 
 
@@ -15,87 +15,91 @@ class TestPlayer(NamedPlayerMixin, BoardPlayerMixin, Player):
 
 class TestTicTacToe(unittest.TestCase):
     """ Test TicTacToe """
-    game = TicTacToeGame()
-    players = (TestPlayer(name="X", board=game.board),)
-    players += (TestPlayer(name="O", board=game.board),)
 
-    game.set_players(players)
+    game = None
+    p1 = None
+    p2 = None
+
+    def __init__(self, *args, **kwargs):
+        """ Sets up the tests """
+        super().__init__(*args, **kwargs)
+
+        self.game = TicTacToeGame()
+
+        self.p1 = TestPlayer(name="X", board=self.game.board)
+        self.p2 = TestPlayer(name="O", board=self.game.board)
+
+        self.game.set_players((self.p1, self.p2))
+
+    def tearDown(self):
+        """ Clears the board after each individual test """
+        self.game.board.clear()
 
     def test_win_horizontal(self):
         """ Test winning horizontal """
-        self.game.board.set(0, 0, self.players[0])
-        self.game.board.set(1, 2, self.players[1])
-        self.game.board.set(0, 2, self.players[0])
-        self.game.board.set(2, 0, self.players[1])
-        self.game.board.set(0, 1, self.players[0])
+        self.game.board.set(0, 0, self.p1)
+        self.game.board.set(1, 2, self.p2)
+        self.game.board.set(0, 2, self.p1)
+        self.game.board.set(2, 0, self.p2)
+        self.game.board.set(0, 1, self.p1)
 
-        self.assertEqual(self.game.has_won_horizontal(self.players[0]), True)
-
-        self.game.board.clear()
+        self.assertEqual(self.game.has_won_horizontal(self.p1), True)
 
     def test_win_vertical(self):
         """ Test winning vertical """
-        self.game.board.set(0, 1, self.players[0])
-        self.game.board.set(1, 2, self.players[1])
-        self.game.board.set(2, 1, self.players[0])
-        self.game.board.set(2, 0, self.players[1])
-        self.game.board.set(1, 1, self.players[0])
+        self.game.board.set(0, 1, self.p1)
+        self.game.board.set(1, 2, self.p2)
+        self.game.board.set(2, 1, self.p1)
+        self.game.board.set(2, 0, self.p2)
+        self.game.board.set(1, 1, self.p1)
 
-        self.assertEqual(self.game.has_won_vertical(self.players[0]), True)
-
-        self.game.board.clear()
+        self.assertEqual(self.game.has_won_vertical(self.p1), True)
 
     def test_win_diagonal1(self):
         """ Test winning diagonal / """
-        self.game.board.set(2, 0, self.players[0])
-        self.game.board.set(1, 2, self.players[1])
-        self.game.board.set(0, 2, self.players[0])
-        self.game.board.set(2, 2, self.players[1])
-        self.game.board.set(1, 1, self.players[0])
+        self.game.board.set(2, 0, self.p1)
+        self.game.board.set(1, 2, self.p2)
+        self.game.board.set(0, 2, self.p1)
+        self.game.board.set(2, 2, self.p2)
+        self.game.board.set(1, 1, self.p1)
 
-        self.assertEqual(self.game.has_won_diagonal(self.players[0]), True)
-
-        self.game.board.clear()
+        self.assertEqual(self.game.has_won_diagonal(self.p1), True)
 
     def test_win_diagonal2(self):
         """ Test winning diagonal \ """
-        self.game.board.set(0, 0, self.players[0])
-        self.game.board.set(1, 2, self.players[1])
-        self.game.board.set(2, 2, self.players[0])
-        self.game.board.set(2, 0, self.players[1])
-        self.game.board.set(1, 1, self.players[0])
+        self.game.board.set(0, 0, self.p1)
+        self.game.board.set(1, 2, self.p2)
+        self.game.board.set(2, 2, self.p1)
+        self.game.board.set(2, 0, self.p2)
+        self.game.board.set(1, 1, self.p1)
 
-        self.assertEqual(self.game.has_won_diagonal(self.players[0]), True)
-
-        self.game.board.clear()
+        self.assertEqual(self.game.has_won_diagonal(self.p1), True)
 
     def test_draw(self):
         """ Test draw """
-        self.game.board.set(0, 0, self.players[0])
-        self.game.board.set(0, 1, self.players[1])
-        self.game.board.set(1, 1, self.players[0])
-        self.game.board.set(0, 2, self.players[1])
-        self.game.board.set(1, 2, self.players[0])
-        self.game.board.set(1, 0, self.players[1])
-        self.game.board.set(2, 0, self.players[0])
-        self.game.board.set(2, 2, self.players[1])
-        self.game.board.set(2, 1, self.players[0])
+        self.game.board.set(0, 0, self.p1)
+        self.game.board.set(0, 1, self.p2)
+        self.game.board.set(1, 1, self.p1)
+        self.game.board.set(0, 2, self.p2)
+        self.game.board.set(1, 2, self.p1)
+        self.game.board.set(1, 0, self.p2)
+        self.game.board.set(2, 0, self.p1)
+        self.game.board.set(2, 2, self.p2)
+        self.game.board.set(2, 1, self.p1)
 
-        self.assertEqual(self.game.state, (2, None))
-
-        self.game.board.clear()
+        self.assertEqual(self.game.state, (STATUS_DRAW, None))
 
     def test_state(self):
         """ Test state of the game """
-        self.game.board.set(0, 0, self.players[0])
-        self.game.board.set(1, 2, self.players[1])
-        self.game.board.set(2, 2, self.players[0])
-        self.game.board.set(2, 0, self.players[1])
-        self.game.board.set(1, 1, self.players[0])
+        self.assertEqual(self.game.state, (STATUS_UNCLEAR, None))
 
-        self.assertEqual(self.game.state, (1, self.players[0]))
+        self.game.board.set(0, 0, self.p1)
+        self.game.board.set(1, 2, self.p2)
+        self.game.board.set(2, 2, self.p1)
+        self.game.board.set(2, 0, self.p2)
+        self.game.board.set(1, 1, self.p1)
 
-        self.game.board.clear()
+        self.assertEqual(self.game.state, (STATUS_WINNER, self.p1))
 
 if __name__ == '__main__':
     unittest.main()
