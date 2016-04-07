0c8becfecef3e714759dc9d7f8500645bb907612
JannesJonkers <jjonkers2@gmail.com>
2016-04-07 11:54:27 +0200

    *Changed __MAIN__ to allow for ai opponents, now with the actual file

--- reversi/__main__.py
+++ reversi/__main__.py
@@ -1,7 +1,7 @@
 """ This class provides the setup for an ReversiGame"""
 
 from game import ReversiGame
-# from ai import AIPlayer
+from ai import AIPlayer
 from players import BoardPlayerMixin, NamedPlayerMixin, Player
 
 
@@ -11,7 +11,7 @@ class DemoCliPlayer(NamedPlayerMixin, BoardPlayerMixin):
 
         try:
             x, y = str(input("Please enter coords to update the board? [x,y] ")).split(',')
-            game.execute_move(self.name[0:1], int(x), int(y))
+            game.execute_move(self, int(x), int(y))
             print("\n")
         except Exception as e:
             print("{}\n".format(e))
@@ -19,15 +19,14 @@ class DemoCliPlayer(NamedPlayerMixin, BoardPlayerMixin):
 
 
 game = ReversiGame()
-players = (DemoCliPlayer(name="W", board=game.board), DemoCliPlayer(name="B", board=game.board),)
+players = (DemoCliPlayer(name="W", board=game.board),)
 
-# answer = str(input("Would you like to play against the computer? y/n"))
-# if answer == "y":
-#     print("Tough luck, that's not yet implemented.")
-#     exit()
-#     players += (AIPlayer(name="B", board=game.board, game=game),)
-# else:
-# players += (DemoCliPlayer(name="B", board=game.board),)
+answer = str(input("Would you like to play against the computer? y/n"))
+if answer == "y":
+    players += (AIPlayer(name="B", board=game.board, game=game),)
+else:
+    players += (DemoCliPlayer(name="B", board=game.board),)
 
 game.set_players(players)
+print(game.get_legal_moves(players[0]))
 game.play()
